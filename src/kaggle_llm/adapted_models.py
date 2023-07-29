fdd3bd142e93f4292f905a26e8330b7e0f4675c8
sumo <sirapoab.c@gmail.com>
2023-07-29 15:11:43 +0100

    got llama in, no way to test it without breaking the gpu yet

--- src/kaggle_llm/adapted_models.py
+++ src/kaggle_llm/adapted_models.py
@@ -1,5 +1,7 @@
 from transformers.models.mt5.configuration_mt5 import MT5Config
-from transformers.models.mt5.modeling_mt5 import MT5Model, MT5Stack, MT5PreTrainedModel
+from transformers.models.mt5.modeling_mt5 import MT5Stack, MT5PreTrainedModel
+from transformers.models.llama.configuration_llama import LlamaConfig
+from transformers.models.llama.modeling_llama import LlamaPreTrainedModel, LlamaModel
 from transformers.modeling_outputs import MultipleChoiceModelOutput
 from transformers import AutoModelForMultipleChoice
 from typing import *
@@ -14,12 +16,6 @@ from transformers.models.deberta_v2.modeling_deberta_v2 import ContextPooler, St
 class MT5ModelForMultipleChoice(MT5PreTrainedModel):
     def __init__(self, config: MT5Config):
         MT5PreTrainedModel.__init__(self, config)
-        config.pooler_hidden_size = getattr(config, "pooler_hidden_size", config.hidden_size)
-        config.pooler_dropout = 0
-        config.pooler_hidden_act = "gelu"
-
-        num_labels = getattr(config, "num_labels", 2)
-        self.num_labels = num_labels
 
         self.shared = nn.Embedding(config.vocab_size, config.d_model)
         encoder_config = copy.deepcopy(config)
@@ -28,15 +24,17 @@ class MT5ModelForMultipleChoice(MT5PreTrainedModel):
         encoder_config.is_encoder_decoder = False
         self.encoder = MT5Stack(encoder_config, self.shared)
 
-        # TODO(Sumo): check if ContextPoolers + StableDropouts are compatible
+        config.pooler_hidden_size = getattr(config, "pooler_hidden_size", config.hidden_size)
+        config.pooler_dropout = 0
+        config.pooler_hidden_act = "gelu"
+        num_labels = getattr(config, "num_labels", 2)
+        self.num_labels = num_labels
         self.pooler = ContextPooler(config)
         output_dim = self.pooler.output_dim
-
         self.classifier = nn.Linear(output_dim, 1)
         drop_out = getattr(config, "cls_dropout", None)
         drop_out = self.config.dropout_rate if drop_out is None else drop_out
         self.dropout = StableDropout(drop_out)
-
         self.model_parallel = False
         self.init_weights()
 
@@ -109,3 +107,98 @@ class MT5ModelForMultipleChoice(MT5PreTrainedModel):
 
 
 AutoModelForMultipleChoice.register(MT5Config, MT5ModelForMultipleChoice, exist_ok=True)
+
+
+# =====
+
+
+class LlamaModelForMultipleChoice(LlamaPreTrainedModel):
+    def __init__(self, config: LlamaConfig):
+        LlamaPreTrainedModel.__init__(self, config)
+        self.shared = nn.Embedding(config.vocab_size, config.d_model)
+        self.model = LlamaModel(config)
+
+        config.pooler_hidden_size = getattr(config, "pooler_hidden_size", config.hidden_size)
+        config.pooler_dropout = 0
+        config.pooler_hidden_act = "gelu"
+        num_labels = getattr(config, "num_labels", 2)
+        self.num_labels = num_labels
+        self.pooler = ContextPooler(config)
+        output_dim = self.pooler.output_dim
+        self.classifier = nn.Linear(output_dim, 1)
+        drop_out = getattr(config, "cls_dropout", None)
+        drop_out = self.config.dropout_rate if drop_out is None else drop_out
+        self.dropout = StableDropout(drop_out)
+        self.model_parallel = False
+        self.init_weights()
+
+    def get_input_embeddings(self):
+        return self.model.get_input_embeddings()
+
+    def set_input_embeddings(self, new_embeddings):
+        self.model.set_input_embeddings(new_embeddings)
+
+    def forward(
+        self,
+        input_ids: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        token_type_ids: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.Tensor] = None,
+        inputs_embeds: Optional[torch.Tensor] = None,
+        labels: Optional[torch.Tensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[Tuple, MultipleChoiceModelOutput]:
+        r"""
+        labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
+            Labels for computing the multiple choice classification loss. Indices should be in `[0, ...,
+            num_choices-1]` where `num_choices` is the size of the second dimension of the input tensors. (See
+            `input_ids` above)
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        num_choices = input_ids.shape[1] if input_ids is not None else inputs_embeds.shape[1]
+
+        flat_input_ids = input_ids.view(-1, input_ids.size(-1)) if input_ids is not None else None
+        flat_position_ids = position_ids.view(-1, position_ids.size(-1)) if position_ids is not None else None
+        # flat_token_type_ids = token_type_ids.view(-1, token_type_ids.size(-1)) if token_type_ids is not None else None
+        flat_attention_mask = attention_mask.view(-1, attention_mask.size(-1)) if attention_mask is not None else None
+        flat_inputs_embeds = (
+            inputs_embeds.view(-1, inputs_embeds.size(-2), inputs_embeds.size(-1))
+            if inputs_embeds is not None
+            else None
+        )
+
+        outputs = self.model.forward(  # TODO(Sumo)
+            input_ids=flat_input_ids,
+            attention_mask=flat_attention_mask,
+            inputs_embeds=flat_inputs_embeds,
+            position_ids=flat_position_ids,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        encoder_layer = outputs[0]
+        pooled_output = self.pooler(encoder_layer)
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+        reshaped_logits = logits.view(-1, num_choices)
+
+        loss = None
+        if labels is not None:
+            loss = torch.nn.functional.cross_entropy(reshaped_logits, labels)
+
+        if not return_dict:
+            output = (reshaped_logits,) + outputs[1:]
+            return ((loss,) + output) if loss is not None else output
+
+        return MultipleChoiceModelOutput(
+            loss=loss,
+            logits=reshaped_logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+
+AutoModelForMultipleChoice.register(LlamaConfig, LlamaModelForMultipleChoice, exist_ok=True)

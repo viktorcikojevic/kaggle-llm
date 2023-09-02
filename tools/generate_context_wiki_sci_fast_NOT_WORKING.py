ea72f457d01aa6e13c5dd91718362af8f42a7dee
viktorcikojevic <cikojevic.viktor@gmail.com>
2023-09-02 11:16:07 +0200

    sumo help

--- tools/generate_context_wiki_sci_fast_NOT_WORKING.py
+++ tools/generate_context_wiki_sci_fast_NOT_WORKING.py
@@ -7,8 +7,10 @@ from sentence_transformers import SentenceTransformer
 import faiss
 import argparse
 import gc
+from sklearn.metrics.pairwise import cosine_similarity
 
-def main(wiki_sci_parquets, model_dir, input_csv, out_dir, out_name, k, max_context_len):
+
+def main(wiki_sci_parquets, model_dir, input_csv, out_dir, out_name, k, max_context_len, faiss_index=None):
     
     
     # you save a model like this
@@ -32,100 +34,112 @@ def main(wiki_sci_parquets, model_dir, input_csv, out_dir, out_name, k, max_cont
     csv['prompt_joined'] = csv[['prompt', 'A', 'B', 'C', 'D', 'E']].apply(lambda x: x['prompt'] + " " + x['A'] + " " + x['B'] + " " + x['C'] + " " + x['D'] + " " + x['E'], axis=1)
     tqdm.pandas()
     csv['embd_prompt'] = csv['prompt_joined'].progress_apply(lambda x: model.encode(x, normalize_embeddings=True))
+    print("Prompt embeddings computed")
+
+    del model
+    gc.collect()
+
 
     # build index from --wiki-sci-parquets directory
     parquet_files = [os.path.join(wiki_sci_parquets, f) for f in os.listdir(wiki_sci_parquets) if f.endswith('.parquet')]
-    wiki_sci_df = pd.read_parquet(parquet_files[0])
-
-    ##
     
     
-    sentence_embeddings = np.stack(wiki_sci_df['sentences_embd'].values).astype('float32')
-    sentence_index = faiss.IndexFlatIP(sentence_embeddings.shape[1])
-    sentence_index.add(sentence_embeddings)
-
-    sentences = list(wiki_sci_df['sentences'].values)
-    
-    del wiki_sci_df, sentence_embeddings
-
-    print("Creating faiss embedding index for wiki sentences ... ")
-    for file_path in tqdm(parquet_files[1:]):
-        df_tmp = pd.read_parquet(file_path)
-
-        sentence_embeddings = np.stack(df_tmp['sentences_embd'].values).astype('float32')
-        sentence_index.add(sentence_embeddings)
-
-        # sentences.extend(list(df_tmp['sentences'].values))
+    # There are 30 parquet files
+    print("Finding sentences for prompts ... ")
+    for pfile_indx, parquet_file in tqdm(enumerate(parquet_files[:2]), total=len(parquet_files)):
         
-        del sentence_embeddings, df_tmp
+        # Create dataframe for a given parquet file
+        wiki_sci_df = pd.read_parquet(parquet_file)
+        sentence_embeddings = np.stack(wiki_sci_df['sentences_embd'].values).astype('float32')
+        
+        d = sentence_embeddings.shape[1]  # dimensionality of the data
+        n_clusters = 1000  # number of clusters
+        niter = 30  # number of iterations
+
+        print(f"Training kmeans with d={d}, k={n_clusters}, niter={niter}")
+        kmeans = faiss.Kmeans(d, n_clusters, niter=niter, verbose=False)
+        kmeans.index = faiss.IndexFlatIP(d)
+        kmeans.train(sentence_embeddings)
+        
+        final_sentences = []
+        final_distances = []
+        
+        for indx, embd_prompt in enumerate(csv['embd_prompt']):
+            
+            distances, indices = kmeans.index.search(embd_prompt.reshape(1, -1), len(parquet_files) // k)
+            sentences = [wiki_sci_df['sentences'].values[i] for i in indices[0]]
+            
+            final_sentences.append(sentences)
+            final_distances.append(distances[0])
+
+        column_name_distances = f"parquet_{pfile_indx}_distances"
+        column_name_sentences = f"parquet_{pfile_indx}_sentences"
+        
+        print(f"Populating columns {column_name_distances} and {column_name_sentences}")
+        
+        csv[column_name_sentences] = final_sentences
+        csv[column_name_distances] = final_distances
+        
+        
+        
+        
+        
+        del wiki_sci_df, sentence_embeddings, distances, indices, kmeans
         gc.collect()
         
         
-    # # get the number of clusters
-    # nlist = sentence_index.ntotal
-    # print(f"Number of clusters: {nlist}")
-    # # get the number of subquantizers   
-    # m = sentence_index.nprobe
-    # print(f"Number of subquantizers: {m}")
-    # return
+    # Now that we have a full csv with all the sentences, we can create the context
+    columns_distances = [c for c in csv.columns if c.endswith('_distances')]
+    columns_distances_n = [int(c.split('_')[1]) for c in columns_distances]
+    sorted_indices = np.argsort(columns_distances_n)
+    columns_distances = [columns_distances[i] for i in sorted_indices]
+
+    columns_sentences = [c for c in csv.columns if c.endswith('_sentences')]
+    columns_sentences_n = [int(c.split('_')[1]) for c in columns_sentences]
+    sorted_indices = np.argsort(columns_sentences_n)
+    columns_sentences = [columns_sentences[i] for i in sorted_indices]
+
+    distances_all = np.stack([np.stack(x) for x in csv[columns_distances].values])
+    sentences_all = np.stack([np.stack(x) for x in csv[columns_sentences].values])
+
+    n_rows, n_parquets, n_sentences = distances_all.shape
+    distances_all = np.reshape(distances_all, (n_rows, n_sentences * n_parquets))
+    sentences_all = np.reshape(sentences_all, (n_rows, n_sentences * n_parquets))
+
+    
+    
+    # take the top k sentences
+    context_sentences = []
+    for i in tqdm(range(len(csv))):
         
         
-    # d = 1024  # dimension
-    # nlist = 1000  # number of clusters
-    # m = 16  # number of subquantizers
-
-    # quantizer = faiss.IndexFlatL2(d)
-    # sentence_index = faiss.IndexIVFPQ(quantizer, d, nlist, m, 8)
-    # # 8 specifies that each sub-vector is encoded as 8 bits
-
-    # sentence_embeddings = np.stack(wiki_sci_df['sentences_embd'].values).astype('float32')
-    # sentence_index.train(sentence_embeddings)
-    # sentence_index.add(sentence_embeddings)
-
-    # sentences = list(wiki_sci_df['sentences'].values)
-
-    # del wiki_sci_df, sentence_embeddings
-
-    # print("Creating faiss embedding index for wiki sentences ... ")
-    # for file_path in tqdm(parquet_files[1:]):
-    #     df_tmp = pd.read_parquet(file_path)
-
-    #     sentence_embeddings = np.stack(df_tmp['sentences_embd'].values).astype('float32')
-    #     sentence_index.train(sentence_embeddings)
-    #     sentence_index.add(sentence_embeddings)
-
-    #     sentences.extend(list(df_tmp['sentences'].values))
-
-    #     del sentence_embeddings, df_tmp
-    #     gc.collect()
-
-
-
-    # iterate over csv and find top k closest sentences
-    # for loop, connect the sentences into one big string, put into csv['context']
-    contexts = []
-    for embd_prompt in tqdm(csv['embd_prompt']):
-        distances, indices = sentence_index.search(embd_prompt.reshape(1, -1), k)
-        best_sentences = [sentences[i] for i in indices[0]]
+        distances = distances_all[i]
+        sentences = sentences_all[i]
+                
+        best_sentences = [sentences[j] for j in np.argsort(distances)[-k:]]
+        # flip the order of sentences
+        best_sentences = best_sentences[::-1]        
         context = '. '.join(best_sentences)
-        context = context.replace("..", ".").replace("\n", " ")
+        context = context.replace("..", ".").replace(". .", ". ")
         context = context[:max_context_len]  # truncate context to max_context_len
-        contexts.append(context)
-
-    csv['context'] = contexts
-
-    # replace csv['prompt'] with
+        context_sentences.append(context)
+    
+    csv['context'] = context_sentences
     csv['prompt'] = csv[['context', 'prompt']].apply(lambda x: 'Context: '+ x['context'] + " ###  " + x['prompt'], axis=1)
-
+    
+    # take only prompt, A, B, C, D, E columns
+    csv = csv[['id', 'prompt', 'A', 'B', 'C', 'D', 'E']]
+    
     # save csv to out-dir
+    out_name = out_name.replace(".csv", "")
     csv.to_csv(os.path.join(out_dir, f"{out_name}.csv"), index=False)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--wiki-sci-parquets", type=str, required=True, default="")
+    parser.add_argument("--wiki-sci-parquets", type=str, required=False, default=None)
     parser.add_argument("--input-csv", type=str, required=True, default="")
-    parser.add_argument("--model-dir", type=str, required=True, default="")
+    parser.add_argument("--model-dir", type=str, required=False, default="")
     parser.add_argument("--out-dir", type=str, required=True, default="")
     parser.add_argument("--out-name", type=str, required=True, default="")
     parser.add_argument("-k", type=int, required=True, default=10)
